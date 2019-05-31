6a129b1c4528e3bcb38bead4723f38d95e723486
任嘉颖 <18832623556@163.com>
2019-05-31 17:29:14 +0800

    Initial commit

---
+++ src/com/atguigu/java/TemplatesTest1.java
@@ -0,0 +1,27 @@
+package com.atguigu.java;
+
+public class TemplatesTest1 {
+//    修改快捷键 psvm改为main
+    public static void main(String[] args) {
+
+
+    }
+    //全局变量
+    public static final int nuym=10;
+//自定义生成模板
+public void testupdate(){
+
+}
+
+//定义私有变量
+/**
+*客户id
+*/
+private int id=10;
+/**
+*name
+*/
+
+
+
+}
